7091f84cfd7bd50fa71e118a838815bde9c14136
DesignrKnight <notify.abelmathew@gmail.com>
2020-09-17 12:03:25 +0530

    Setup Comment Workflow

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {Octokit}=require('@octokit/core');
+
+const makeComment=async (githubToken,url,number,body)=>{
+  const octokit=new Octokit({auth:githubToken})
+  const newComment = await octokit.request('POST '+url, {    
+    issue_number: number,
+    body: body
+  })
+  console.log(newComment);
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hey ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+  const githubToken = core.getInput('GITHUB_TOKEN');
+
+  const url=context.payload.pull_request.comments_url.substr(22);
+  if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+  }
+  const pull_request_number = context.payload.pull_request.number;
+  makeComment(githubToken,url,pull_request_number,nameToGreet);
+  const payload = JSON.stringify(github.context.payload)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
