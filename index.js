9b969075c13fc01e797cf414e62eef878eac8d08
Abel Mathew <notify.abelmathew@gmail.com>
2020-09-17 17:11:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ try {
   //makeComment(githubToken,url,pull_request_number,nameToGreet);
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
-  exec(`depcheck ${dir}`, (error, stdout, stderr) => {
+  exec(`depcheck ${dir} && bundle-phobia -p ${dir}package.json`, (error, stdout, stderr) => {
     makeComment(githubToken,url,pull_request_number,stdout);
     if (error) {
         console.log(`error: ${error.message}`);
